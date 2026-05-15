bdbe45d760d8c7717c75d539dc10b756243e5a9d
Lavanya <lavanyasinha50@gmail.com>
2026-05-15 17:52:31 +0530

    render port fix

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request, render_template
 from similarity import calculate_similarity
 from flask import render_template
 from similarity import calculate_similarity, sentence_similarity
+import os
 
 app = Flask(__name__)
 
@@ -43,9 +44,12 @@ def analyze():
     ]
 })
 
-     
 if __name__ == "__main__":
-    app.run(debug = "True")
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)    
+
 
     
     
